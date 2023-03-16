2a8a3fa4bb87629d5ce2be9d7f2b2614ba376060
Miloserdie <89547280+Miloserdie@users.noreply.github.com>
2023-03-17 01:34:05 +0200

    create header component and burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from "./Header";
 
 function App() {
   return (
     <>
-
+      <Header />
     </>
   );
 }
--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,8 +1,33 @@
-
+import burgerTitle from '../assets/burger-title.png'
+import './style.scss';
+import {useState} from "react";
 const Header = () => {
+    const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+    const activeMenu = isMenuActive ? 'active' : '';
+
+    const handleBurgerBtnClick = () => {
+        setIsMenuActive(prevState => !prevState);
+    }
+
     return (
-        <header>
-            <img src="" alt="burger-title"/>
+        <header className={'header'}>
+            <div className={'header__container container'}>
+                <div className={'header__left'}>
+                    <button onClick={handleBurgerBtnClick} className={'header__burger-btn'}>
+                        <span />
+                    </button>
+                    <img className={`header__title-img ${activeMenu}`} src={burgerTitle} alt="burger-title"/>
+                </div>
+                <nav onClick={handleBurgerBtnClick} className={`header__navigation-bar ${activeMenu}`}>
+                    <ul className={activeMenu}>
+                        <li><a href="#">INGREDIENT</a></li>
+                        <li><a href="#">STORY</a></li>
+                        <li><a href="#">BURGERS</a></li>
+                        <li><a href="#">LOCATION</a></li>
+                    </ul>
+                </nav>
+                <a className={'header__link'} href="#">Order now</a>
+            </div>
         </header>
     );
 };
